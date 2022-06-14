1e29cec5f14223a5ed81fc1b9daeeb58c2fbfd2b
Rafael Augusto Pacheco Baiao <bbpt0101@bbp.com.br>
2022-06-14 14:13:58 -0300

    setup project and add styled-componts

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
